68a9359363a93ce7871efa8e2c13f1ba1737922a
Mijken <michelmarchoni@gmail.com>
2021-10-01 16:38:22 -0300

    actualizacion

---
+++ desktop/src/com/bws/tanks/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bws.tanks.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bws.tanks.Tanks;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Tanks(), config);
+	}
+}
