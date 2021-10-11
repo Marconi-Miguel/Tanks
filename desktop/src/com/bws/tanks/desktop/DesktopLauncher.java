e29998b799110919ccac850d362a4f9421243b6e
Mijken <michelmarchoni@gmail.com>
2021-10-11 17:10:07 -0300

    resizable

--- desktop/src/com/bws/tanks/desktop/DesktopLauncher.java
+++ desktop/src/com/bws/tanks/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.bws.tanks.Tanks;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
 		new LwjglApplication(new Tanks(), config);
 		//a
 	}
