9270a8380e905fd081546897069a8f1bdd825d5a
Mijken <michelmarchoni@gmail.com>
2021-10-12 16:20:38 -0300

    update Projectile
    hice que extendiera de imageClass ya que posee projectile utiliza varias de las propiedades, aparte de que vamos a necesitar setear el tamaño de la mayoria de los rendereables

--- core/src/elements/Projectile.java
+++ core/src/elements/Projectile.java
@@ -6,37 +6,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Projectile extends ImageClass{
 	
-	Texture texture;
-	Sprite sprite;
+	
 	public float rotation;
 	public float speed;
 	//TODO register parent tank
 	
 
 	public Projectile(float x, float y, float rotation, String texture, float speed){
-		this.texture = new Texture(texture);	
-		sprite = new Sprite(this.texture);
+		super(texture);
+		
 		this.rotation += rotation;
-		sprite.setX(x);
-		sprite.setY(y);
+		s.setX(x);
+		s.setY(y);
 		this.speed = speed;
 	}
 	
-	public void Render(SpriteBatch batch) {
+	public void draw() {
+		super.draw();
 		doMovement();
-		sprite.draw(batch);
 	}
 	
-	void setPosition(float x, float y) {
-		sprite.setX(x);
-		sprite.setY(y);
-	}
+	
 	
 	void doMovement() {
 		float tempX = (float) Math.cos(Math.toRadians(rotation) ) * speed;
 		float tempY = (float) Math.sin(Math.toRadians(rotation) ) * speed;
-		setPosition(sprite.getX() + tempX * -1, sprite.getY() + tempY * -1);
-		sprite.setRotation(rotation+90);
+		setPosition(s.getX() + tempX * -1, s.getY() + tempY * -1);
+		s.setRotation(rotation+90);
 	}
 	
 }
