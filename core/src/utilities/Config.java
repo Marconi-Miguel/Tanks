4e78c95d28253422f1e40a76fa91362515dc5cd7
Mijken <michelmarchoni@gmail.com>
2021-10-12 16:33:12 -0300

    Borro clase y cambio random
    borro la clase Image, tenemos clase ImageClass y hago cambio de config.

--- core/src/utilities/Config.java
+++ core/src/utilities/Config.java
@@ -8,12 +8,9 @@ public abstract class Config {
     
     // filtros utilizando BITWISE:
     public static final short DEFAULT_BIT = 1;
-    public static final short ENTIDAD_BIT = 2;
-    public static final short COFRE_BIT = 4;
+    public static final short BARRIL_BIT = 2;
+    public static final short BOLSA_BIT = 4;
     public static final short SALIDA_BIT = 8;
-    public static final short ABIERTO_BIT = 16;
-    public static final short PJ_BIT = 32;
-    public static final short ESCALABLE_BIT = 64;
-    public static final short LIBRE_BIT = 128;
+    
     // 0000 1100
 }
