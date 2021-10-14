339e4f2fe6e0e37eb50b78a99d54f10b77291fbe
Mijken <michelmarchoni@gmail.com>
2021-10-13 22:23:36 -0300

    tiledMap

--- core/src/tiledObjects/Camino.java
+++ core/src/tiledObjects/Camino.java
@@ -2,17 +2,14 @@ package tiledObjects;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.physics.box2d.World;
 
-import utiles.Config;
-
-public class Escalable extends InteractiveObject {
+public class Camino extends InteractiveObject {
 	
-	public Escalable( TiledMap map, Rectangle limites) {
+	public Camino( TiledMap map, Rectangle limites) {
 		super( map, limites);
 		//crea su caja 2d
 		fixture.setUserData(this);
-		definirFiltro(Config.ESCALABLE_BIT);
+		
 	}
 
 	@Override
