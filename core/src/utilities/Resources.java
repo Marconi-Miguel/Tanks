7bf79242f51f74e3bbd30e2adaaaa4f4114d85c1
Mijken <michelmarchoni@gmail.com>
2021-10-18 18:52:41 -0300

    borro cositas inutiles

--- core/src/utilities/Resources.java
+++ core/src/utilities/Resources.java
@@ -11,12 +11,8 @@ public class Resources {
 	//MAPS
 	public static final String MAP1= "Maps/mapaDessert.tmx";
 	//HULLS /////////////////////////////////////////
-	public static final String BASICHULLBLUE = "Tanks/Blue/tankBody_blue_outline.png";
-	public static final String BASICHULLDARK= "Tanks/Blue/tankBody_blue_outline.png";
-	public static final String BASICHULLGREEN = "Tanks/Blue/tankBody_blue_outline.png";
-	public static final String BASICHULLRED= "Tanks/Blue/tankBody_blue_outline.png";
-	public static final String BASICHULLSAND = "Tanks/Blue/tankBody_blue_outline.png";
-	
+	public static final String BASICHULL = "Tanks/Blue/tankBody_blue_outline.png";
+
 	//CANNONS /////////////////////////////////////////
 	public static final String BASICCANNON = "images/cannons/basic.png";
 	public static final String BASICCANNON_FIRE = "images/cannons/basic_fire.png";
@@ -28,28 +24,5 @@ public class Resources {
 	public static final String BASICSHELL = "images/shells/basic.png";
 
 	
-//	public static TextureRegion getTextureRegion(int color, String ruta) {
-//		TextureRegion region;
-//		switch (color) {
-//		case 1:
-//			atlas = new TextureAtlas(BOTONESAZUL);
-//			region = atlas.findRegion(ruta);
-//			return region;
-//		case 2:
-//
-//			atlas = new TextureAtlas(BOTONESROJO);
-//			region = atlas.findRegion(ruta);
-//			return region;
-//		case 3:
-//			atlas = new TextureAtlas(BOTONESVERDE);
-//			region = atlas.findRegion(ruta);
-//			return region;
-//		case 4:
-//			atlas = new TextureAtlas(BOTONESGRIS);
-//			region = atlas.findRegion(ruta);
-//			return region;
-//		default:
-//			return null;
-//		}
-//	}
+
 }
