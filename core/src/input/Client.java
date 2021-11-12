0e79c6b2ba5b4f000cc393cba9af9dba81fc7e02
SirTosky <dylantoscano.arg@gmail.com>
2021-11-12 01:21:34 -0300

    Create a client class from which clients extend (local & client)

---
+++ core/src/input/Client.java
@@ -0,0 +1,14 @@
+package input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import elements.Tank;
+
+public class Client {
+	
+	public String username;
+	Tank tank;
+	public Map<InputKeys, Boolean>  inputs = new HashMap<InputKeys, Boolean>();
+
+}
