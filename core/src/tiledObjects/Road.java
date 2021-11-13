2aff3fbd0d7c34ae5055c9180d1aa8d5e0d92a78
Mijken <michelmarchoni@gmail.com>
2021-11-13 11:25:41 -0300

    cambie el filtro
    settie que el road tengo el filtro bit del camino, el 4 si mal no recuerdo

--- core/src/tiledObjects/Road.java
+++ core/src/tiledObjects/Road.java
@@ -1,8 +1,9 @@
 package tiledObjects;
 
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
+
+import utilities.Config;
 
 public class Road extends InteractiveObject {
 	
@@ -10,16 +11,15 @@ public class Road extends InteractiveObject {
 		super( map, limites);
 		//crea su caja 2d
 		fixture.setSensor(true);
-		fixture.setUserData(this);
+		fixture.setUserData("Road");
+		setFilter(Config.ROAD_BIT);
 		
 	}
 
 
 	@Override
 	public void interaction() {
-		System.out.println("Escalable: colision");
-		
-		
+		System.out.println("road: colision");
 	}
 
 }
