1bb98d7aa02cebbad7c1167f7a76bdcde99c780b
Mijken <michelmarchoni@gmail.com>
2021-11-15 17:13:45 -0300

    REEMPLAZA PAREDES POR OBSTACULOS
    el default seran los bordes del mapa

--- core/src/utilities/Config.java
+++ core/src/utilities/Config.java
@@ -3,6 +3,7 @@ package utilities;
 public abstract class Config {
 	public static int HEIGHT= 720;
     public static int WIDTH = 1080;
+    public static float delta;
     // PIXELPERMETER la escala, un pixel a la imagen, para que las velocidades y otras propiedades tengan sentido
     public static final float PPM = 100;
     
@@ -10,7 +11,7 @@ public abstract class Config {
     public static final short DEFAULT_BIT = 1;
     public static final short TANK_BIT = 2;
     public static final short ROAD_BIT = 4;
-    public static final short WALL_BIT = 8;
+    public static final short OBSTACLE_BIT = 8;
     public static final short TNKSENSOR_BIT = 16;
     public static final short PROJECTIL_BIT = 32;
     public static final short EXPLOSION_BIT = 64;
