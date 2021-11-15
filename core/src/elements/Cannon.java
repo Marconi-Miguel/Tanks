f5b0277c3cd882be2a5ab2aa2f066ca697bfb43e
Mijken <michelmarchoni@gmail.com>
2021-11-15 17:26:38 -0300

    (inProgress) borro cosas, para adaptar al juego

--- core/src/elements/Cannon.java
+++ core/src/elements/Cannon.java
@@ -1,57 +1,44 @@
 package elements;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
 
 import utilities.Render;
 import utilities.Resources;
 
 public class Cannon extends Attachable {
-	
+
 	public boolean ready = true;
 	public int reloadTime;
-	
+	public ArrayList<Projectile> projectiles = new ArrayList<Projectile>(); 
 	Texture idle;
 	Texture fire;
 	public Sound fireSfx;
-	
+
 	Timer timer = new Timer();
-	
+
 	public Cannon(String idle, String fire) {
 		super(idle);
 		this.idle = new Texture(idle);
-		this.fire = new Texture (fire);
+		this.fire = new Texture(fire);
 		objectType = "Cannon";
 	}
-	
+
 	public void trigger() {
-		if (ready){
+		if (ready) {
 			ready = false;
-			super.modifyTexture(fire);
+			modifyTexture(fire);
 //			fireSfx.play(1,Functions.randomFloat(0.8f,1.2f), 1);
-			Timer.schedule(new Task() {
-				public void run() {
-					idleCannon();
-				}
-			}, 0.03f);
-			Sprite shell = new Projectile(getX() + 45, getY() +80, tank.rotation, Resources.BASICSHELL, 1);
-			Render.addSprite(shell);
-			Timer.schedule(new Task() {
-				public void run() {
-					ready = true;
-				}
-			}, reloadTime);
-			
-		}else {
+			Projectile shell = new Projectile(getX(), getY(), tank, Resources.BASICSHELL, 1);
+			projectiles.add(shell);
 		}
 	}
-	
+
 	private void idleCannon() {
-		super.modifyTexture(idle);
+		modifyTexture(idle);
 	}
-	
 
 }
