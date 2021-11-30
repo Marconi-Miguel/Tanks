9142fd25e5f7384ca78c93bcc58b8fb46c42a6e4
SirTosky <dylantoscano.arg@gmail.com>
2021-11-30 15:06:06 -0300

    chad ClientSprite

--- core/src/elements/ClientSprite.java
+++ core/src/elements/ClientSprite.java
@@ -6,19 +6,6 @@ public class ClientSprite extends Sprite {
 	//Yes. We made an entire class just to add an int, because Marconi.
 	int id;
 	
-	public ClientSprite() {
-	}
-	
-	
-	private void drawSprite(String sprite) { //sprite == rocketbuf
-		
-		//TODO: Do the texture here.
-	}
-	
-	private void deleteSprite() {
-		
-	}
-	
 	public void setID(int id) {
 		this.id = id;
 	}
