c88ae3d94e2f16029a10d1125c3e901d65c9a501
SirTosky <dylantoscano.arg@gmail.com>
2021-12-01 00:00:16 -0300

    Receive OriginX and Y for sprites

--- core/src/utilities/ClientRender.java
+++ core/src/utilities/ClientRender.java
@@ -33,16 +33,15 @@ public abstract class ClientRender {
 		for (int i = 0; i < renderAnimationList.size(); i++) {
 			if (renderAnimationList.get(i) != null) {
 				renderAnimationList.get(i).draw(batch);
-					renderAnimationList.get(i).update();
-					if(renderAnimationList.get(i).end) {
-						renderAnimationList.remove(i);
-					}
-				
+				renderAnimationList.get(i).update();
+				if (renderAnimationList.get(i).end) {
+					renderAnimationList.remove(i);
+				}
+
 			}
 		}
 		batch.end();
 	}
-
 
 	public static void disposeList() {
 		for (int i = 0; i < renderList.size(); i++) {
@@ -66,36 +65,38 @@ public abstract class ClientRender {
 		sprite.setX(Float.parseFloat(args[2]));
 		sprite.setY(Float.parseFloat(args[3]));
 		sprite.setRotation(Float.parseFloat(args[4]));
-		sprite.setSize(Float.parseFloat(args[5]),Float.parseFloat(args[6]));
+		sprite.setSize(Float.parseFloat(args[5]), Float.parseFloat(args[6]));
 		renderList.add(sprite);
 	}
+
 	public static void addAnimation(String[] args) {
 		ClientAnimation animation = new ClientAnimation(Float.parseFloat(args[0]), Float.parseFloat(args[1]));
 		renderAnimationList.add(animation);
-		
+
 	}
-
-
 
 	public static void updateSprite(String[] args) {
 		ClientsideSprite sprite = null;
 		for (int i = 0; i < renderList.size(); i++) {
-			
+
 			if (renderList.get(i).getID() == Integer.parseInt(args[1])) {
 				sprite = renderList.get(i);
 				break;
 			}
 		}
 		if (sprite != null) {
-			
+
 			try {
 				sprite.setX(Float.parseFloat(args[2]));
 				sprite.setY(Float.parseFloat(args[3]));
 				sprite.setRotation(Float.parseFloat(args[4]));
-				sprite.setSize(Float.parseFloat(args[5]),Float.parseFloat(args[6]));
-			}catch(NumberFormatException e) {
+				sprite.setSize(Float.parseFloat(args[5]), Float.parseFloat(args[6]));
+				if (Float.parseFloat(args[4]) != 0) {
+					sprite.setOrigin(Float.parseFloat(args[7]), Float.parseFloat(args[8]));
+				}
+			} catch (NumberFormatException e) {
 			}
-			
+
 		}
 	}
 
@@ -107,8 +108,5 @@ public abstract class ClientRender {
 			}
 		}
 	}
-
-
-	
 
 }
